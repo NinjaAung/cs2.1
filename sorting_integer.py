3e1fca087d32317063f387bc98035c33816ae850
NinjaAung <ninja.aung02@gmail.com>
2021-02-17 21:06:43 -0800

    Finsihed Bucket Sort

--- sorting_integer.py
+++ sorting_integer.py
@@ -21,8 +21,7 @@ def what_sort(numbers):
 def counting_sort(numbers):
     """Sort given numbers (integers) by counting occurrences of each number,
     then looping over counts and copying that many numbers into output list.
-    TODO: Running time: O(n+k) Average Case when there is a small semi small range
-    TODO: Memory usage: O(n+k) Average Case when there is a small semi small range"""
+    Running time & Memory usage: O(n+k) Average Case when there is a small semi small range we are manipulating and looping though different of diffrent sizes"""
     min = max = numbers[0]
 
     # find min / max values
@@ -32,7 +31,9 @@ def counting_sort(numbers):
         if numbers[i] > max:
             max = numbers[i]
 
-    # Counting
+    #
+    #  Counting
+     
     k  = 0
     lst = [0] * (max+1)
     for i in range(min,max+1):
@@ -66,11 +67,30 @@ def counting_sort(numbers):
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
     then sorting each bucket and concatenating all buckets in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum values)
-    # TODO: Create list of buckets to store numbers in subranges of input range
-    # TODO: Loop over given numbers and place each item in appropriate bucket
-    # TODO: Sort each bucket using any sorting algorithm (recursive or another)
-    # TODO: Loop over buckets and append each bucket's numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    TODO: Running time & Space Complexity: O(n+k) Average Case whenl managing small enough range, we are looping and manipulating two list a time at differnt sizes"""
+    min = max = numbers[0]
+
+    # find min / max values
+    for i in range(0,len(numbers)):
+        if numbers[i] < min:
+            min = numbers[i]
+        if numbers[i] > max:
+            max = numbers[i]
+    buckets =[ [] for _ in range(0,num_buckets)] # don't want to duplicate refrences
+    bucket_range = (max-min)//num_buckets
+    if bucket_range <= 0:
+        bucket_range = 1
+
+    # Add nums to buckets
+    for i in range(0,len(numbers)):
+        bucket = (numbers[i] - min) // bucket_range
+        buckets[bucket].append(numbers[i])
+    
+    # mutating list
+    k = 0
+    for i in range(0,len(buckets)):
+        if len(buckets[i]) > 0:
+            buckets[i].sort() # I can use any might as well use a built-in
+            for j in range(0,len(buckets[i])):
+                numbers[k] = buckets[i][j]
+                k += 1
